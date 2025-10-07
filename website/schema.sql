e4aaafb72f4ea9da0475c79945bfb082f8e1323e
prabhas <prabhasvegi9392@gmail.com>
2025-10-07 14:12:04 +0530

    🗃️ Added SQL schema file to represent database

---
+++ website/schema.sql
@@ -0,0 +1,15 @@
+-- SQL Database Schema for Flask Notes App
+CREATE TABLE user (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    email TEXT NOT NULL UNIQUE,
+    first_name TEXT NOT NULL,
+    password TEXT NOT NULL
+);
+
+CREATE TABLE note (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    data TEXT NOT NULL,
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    user_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user (id)
+);
